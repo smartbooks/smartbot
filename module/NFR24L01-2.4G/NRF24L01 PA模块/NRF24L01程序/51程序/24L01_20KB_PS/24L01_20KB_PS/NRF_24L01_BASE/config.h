818eef74b1b807b51da4a834a13129f9b6a53b51
smartbooks <smartbooks@qq.com>
2022-06-28 13:17:13 +0800

    update

--- module/NFR24L01-2.4G/NRF24L01 PA模块/NRF24L01程序/51程序/24L01_20KB_PS/24L01_20KB_PS/NRF_24L01_BASE/config.h
+++
@@ -1,8 +0,0 @@
-#include "STC5A60S2.H"
-
-#include"NRF_24L01.H"
-
-#include"UART.H"
-
-sbit KEY = P1^2;
-
--- module/NFR24L01-2.4G/NRF24L01 PA模块/官方测试平台与程序/nRF24L01P/FWLib/typedef.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TYPEDEF_H
-#define __TYPEDEF_H
-
-
-//typedef unsigned char u8;
-
-#endif
